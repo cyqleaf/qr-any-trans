461a1f4f3b499919f61dae2b4bb70d0081084926
hongxiao95 <hongxiao95@hotmail.com>
2022-05-24 15:41:38 +0800

    transferbase

---
+++ qranytrans.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+
+import tkinter as tk 
+
+def main():
+
+    main_win = tk.Tk()
+    main_win.wm_title("任意传输器")
+
+    main_win.mainloop()
+
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ transfer/TransConfigBase.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+
+class TransferBase():
+    '''
+    传输基础类，定义了传输的基本动作，不含实际逻辑。传输基本动作在其子类中
+    '''
+    def __init__(self):
+        pass
+
+    def loadConfig(self):
+        pass
+
+
+class ConfigBase():
+    '''
+    配置基础类，定义了一些配置
+    '''
+    def __init__(self):
+        pass
+
+
+    

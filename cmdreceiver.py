930dddb371211e53402f945805d50e6bfecdf513
hongxiao95 <hongxiao95@hotmail.com>
2022-07-06 17:26:33 +0800

    fix bugs

--- cmdreceiver.py
+++ cmdreceiver.py
@@ -1,4 +1,5 @@
 #coding:utf-8
+from ast import Bytes
 import base64
 from hashlib import md5
 import hashlib
@@ -7,15 +8,94 @@ import cv2
 from pyzbar import pyzbar
 import json
 import traceback
+from io import BytesIO
 
 
 class DecodeInfo():
+    TMP_FILE_TYPE = ".qtt"
     def __init__(self):
         self.rec_file_name = ""
         self.file_md5 = ""
         self.total_frame_count = 0
         self.file_bytes_buffer = []
         self.miss_frame_indexes = []
+
+    def write_full_file(self):
+        file_complete = True
+        bio = BytesIO()
+        with open(self.rec_file_name, "wb") as rec_file:
+            for i in range(len(self.file_bytes_buffer)):
+                cur_bytes = bytes(self.file_bytes_buffer[i])
+                rec_file.write(cur_bytes)
+                bio.write(cur_bytes)
+                if len(self.file_bytes_buffer[i]) == 0:
+                    file_complete = False
+            
+        bio.seek(0, 0)
+        decode_md5 = hashlib.md5(bio.read()).hexdigest()
+
+        print(f"文件 [{self.rec_file_name}] 已写入。{'但由于有丢帧，不保证准确性' if not file_complete else ''}")
+        print(f"已校验文件MD5, {'与源文件一致！' if decode_md5.strip() == self.file_md5 else '与源文件有出入，请谨慎采纳'}。 接收文件MD5:{[decode_md5]}\n 源文件的MD5:{self.file_md5}")
+
+    def write_tmp_file(self):
+        if len(self.miss_frame_indexes) == 0:
+            print("Warning: 文件已无丢帧情况，但仍将按要求写入临时文件")
+
+        # 临时文件结构:base64(json) 
+        '''
+        第一层base64解码后：
+        {rec_file_name:"xx", file_md5:"xx", total_frame_count:"", self.file_bytes_buffer:{1:[base64],2:[base64]}, miss_frame_indexes:[number,number] }
+        '''
+        # 先制作主数据buffer
+        buffer_dict_base_64 = dict()
+
+        for i in range(len(self.file_bytes_buffer)):
+            if len(self.file_bytes_buffer[i]) > 0:
+                buffer_dict_base_64[i] = base64.b64encode(self.file_bytes_buffer[i]).decode("utf-8")
+        
+        tmp_file_dict = dict()
+        tmp_file_dict["rec_file_name"] = self.rec_file_name
+        tmp_file_dict["file_md5"] = self.file_md5
+        tmp_file_dict["total_frame_count"] = self.total_frame_count
+        tmp_file_dict["file_bytes_buffer"] = buffer_dict_base_64
+        tmp_file_dict["miss_frame_indexes"] = self.miss_frame_indexes
+
+        tmp_file_dict_json = json.dumps(tmp_file_dict)
+
+        tmp_file_base64 = base64.b64encode(tmp_file_dict_json.encode("utf-8"))
+
+        tmp_file_name = self.rec_file_name + DecodeInfo.TMP_FILE_TYPE
+        with open(tmp_file_name, "wb") as tmp_file:
+            tmp_file.write(tmp_file_base64)
+
+        print(f"已写入临时文件 [{tmp_file_name}] 。")
+
+def read_from_tmp_file(tmp_file_name:str, decode_info: DecodeInfo):
+    tmp_file_dict = dict()
+
+    with open(tmp_file_name, "rb") as tmp_file:
+        tmp_file_bytes = tmp_file.read()
+        tmp_file_json = base64.b64decode(tmp_file_bytes).decode("utf-8")
+
+        tmp_file_dict = json.loads(tmp_file_json)
+
+    decode_info.rec_file_name = tmp_file_dict["rec_file_name"]
+    decode_info.file_md5 = tmp_file_dict["file_md5"]
+    decode_info.total_frame_count = int(tmp_file_dict["total_frame_count"])
+    decode_info.miss_frame_indexes = tmp_file_dict["miss_frame_indexes"]
+
+    decode_info.file_bytes_buffer = [[] for i in range(decode_info.total_frame_count)]
+
+    for key in tmp_file_dict["file_bytes_buffer"].keys():
+        decode_info.file_bytes_buffer[int(key)] = base64.b64decode(tmp_file_dict["file_bytes_buffer"][key])
+
+    if len(decode_info.miss_frame_indexes) > 0:
+        print(f"识别临时文件后总丢帧情况：{decode_info.miss_frame_indexes}")
+        return False
+    else:
+        print(f"识别临时文件完成无丢帧")
+        return True
+
 
 def main():
     print(sys.argv)
@@ -32,7 +112,7 @@ def main():
             return
 
     # 主视频文件名
-    main_video_file_name = sys.argv[1]
+    main_file_name = sys.argv[1]
     # file_name = "~/Downloads/IMG_0560.MOV"
 
     # 补丁视频文件名
@@ -42,10 +122,17 @@ def main():
     decode_info = DecodeInfo()
     
     try:
-        finish = decode_frames(main_video_file_name, False, decode_info=decode_info)
+        finish = True
+        if main_file_name.strip().endswith(".qtt"):
+            finish = read_from_tmp_file(main_file_name, decode_info=decode_info)
+        else:
+            finish = decode_frames(main_file_name, False, decode_info=decode_info)
+
+        print("检查补丁文件。。。" if len(patch_files) > 0 else "")
         for patch_file_name in patch_files:
             if finish is True:
                 break
+            print(f"检查补丁文件 [{patch_file_name}]")
             finish = decode_frames(patch_file_name, True, decode_info=decode_info)
 
         if decode_info.rec_file_name.strip() == "":
@@ -53,13 +140,11 @@ def main():
             return
         
         if finish == False:
-            print(f"仍有丢帧情况，但文件将已如实写入，请检查能否打开")
-
-        with open(decode_info.rec_file_name, "wb") as rec_file:
-            for i in range(len(decode_info.file_bytes_buffer)):
-                rec_file.write(bytes(decode_info.file_bytes_buffer[i]))
-
-        print(f"文件已写入")
+            print(f"仍有丢帧情况，但文件将如实写入，同时写入临时文件，请检查能否打开")
+            decode_info.write_tmp_file()
+        
+        decode_info.write_full_file()
+        
 
     except Exception as e:
         print(f"解析失败, {e}")
